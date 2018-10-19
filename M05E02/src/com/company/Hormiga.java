5a64852b8a16b287e1b5e7db1dd4a780601a1595
Itejada <ismael.elpuig@gmail.com>
2018-10-19 18:29:56 +0200

    Yeah

---
+++ M05E02/src/com/company/Hormiga.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Hormiga {
+    public String color;
+    public String tamaño;
+    private String casta;
+
+    public void caminar(){
+
+    }
+
+    public void  comer(){
+        System.out.println("nñamm ñamñmañmammm");
+
+    }
+    public void  comer(String text){
+        System.out.println(text);
+
+    }
+}
